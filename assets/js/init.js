2ac4b14b578dac7bc21b262671ecb14cd0f58356
Ric Arcila <ricartomojo@gmail.com>
2018-07-06 09:40:37 +0100

    Dataset mapping page

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,10 +1,14 @@
- (function($){
-   $(function(){
+ var standardList;
+
+ (function ($) {
+   $(function () {
 
      $('.button-collapse').sideNav();
      $('.parallax').parallax();
      $('.collapsible').collapsible();
-     $('.carousel.carousel-slider').carousel({fullWidth: true});
+     $('.carousel.carousel-slider').carousel({
+       fullWidth: true
+     });
      $('.materialboxed').materialbox();
      $('.scrollspy').scrollSpy();
      $('.tap-target').tapTarget('open');
@@ -13,10 +17,33 @@
        $('#cookies').hide()
      }
 
-    //  jQuery('#cookies').on('click', function(event) {
-    //         localStorage.setItem('cookieconsent', 'true')
-    //         jQuery('#cookies').toggle('hide');
-    //    });
+     //  jQuery('#cookies').on('click', function(event) {
+     //         localStorage.setItem('cookieconsent', 'true')
+     //         jQuery('#cookies').toggle('hide');
+     //    });
+
+
+     var options = {
+       valueNames: ['name']
+     };
+
+     standardList = new List('standards-list', options);
+
+     $('#dataset .project-filter').click(function (button) {
+       var standard = $(button.target).text();
+       if (standard === 'All') {
+         resetList();
+         return
+       }
+       standardList.search(standard);
+     });
 
    }); // end of document ready
  })(jQuery);
+
+ function resetList() {
+   this.standardList.search();
+   this.standardList.filter();
+   this.standardList.update();
+   //console.log('Reset Successfully!');
+ };
